0fd052cc2bf8f0238ca8194a7a38ddf40f424f3b
David Björklund <david.bjorklund@gmail.com>
2019-11-16 13:52:29 +0100

    updateFn & insertFn should both be optional

--- lib/index.ts
+++ lib/index.ts
@@ -1,24 +1,36 @@
+type UpdateFnType<Value> = (old: Value) => Value;
+type InsertFnType<Value> = () => Value;
+
 export function upsert<Key, Value>(
   map: Map<Key, Value>,
   key: Key,
-  updateFn: (old: Value) => Value,
-  insertFn: () => Value
+  updateFn?: UpdateFnType<Value>,
+  insertFn?: InsertFnType<Value>
 ): Value;
 
 export function upsert<Key extends object, Value>(
   map: WeakMap<Key, Value>,
   key: Key,
-  updateFn: (old: Value) => Value,
-  insertFn: () => Value
+  updateFn?: UpdateFnType<Value>,
+  insertFn?: InsertFnType<Value>
 ): Value;
 
 export function upsert<Key extends object, Value>(
   map: Map<Key, Value> | WeakMap<Key, Value>,
   key: Key,
-  updateFn: (old: Value) => Value,
-  insertFn: () => Value
+  updateFn?: UpdateFnType<Value>,
+  insertFn?: InsertFnType<Value>
 ) {
-  const value = map.has(key) ? updateFn(map.get(key) as Value) : insertFn();
+  const hasKey = map.has(key);
+  let value: Value;
+
+  if (hasKey && updateFn) {
+    value = updateFn(map.get(key) as Value);
+  } else if (!hasKey && insertFn) {
+    value = insertFn();
+  } else {
+    return map.get(key);
+  }
 
   map.set(key, value);
 
--- test.ts
+++ test.ts
@@ -1,22 +1,53 @@
 import assert from "assert";
 import { upsert } from "./lib";
-class Foo {
-  constructor(public bar: number) {}
-}
-const foo1 = new Foo(1);
-const foo2 = new Foo(2);
-
-const map = new Map<string, number>();
 
 const updateFn = (old: number): number => old + 1;
 const insertFn = () => 0;
 
-assert.strictEqual(upsert(map, "foo", updateFn, insertFn), 0);
-assert.strictEqual(upsert(map, "foo", updateFn, insertFn), 1);
-assert.strictEqual(upsert(map, "bar", updateFn, insertFn), 0);
+const test = (name: string, fn: () => void) => {
+  console.log(name);
+  fn();
+  console.log("");
+};
 
-const weakMap = new WeakMap<Foo, number>();
+test("Map", () => {
+  const map = new Map<string, number>();
 
-assert.strictEqual(upsert(weakMap, foo1, updateFn, insertFn), 0);
-assert.strictEqual(upsert(weakMap, foo1, updateFn, insertFn), 1);
-assert.strictEqual(upsert(weakMap, foo2, updateFn, insertFn), 0);
+  assert.strictEqual(upsert(map, "foo", updateFn, insertFn), 0);
+  assert.strictEqual(upsert(map, "foo", updateFn, insertFn), 1);
+  assert.strictEqual(upsert(map, "bar", updateFn, insertFn), 0);
+
+  assert.strictEqual(map.get("foo"), 1);
+  assert.strictEqual(map.get("bar"), 0);
+});
+
+test("WeakMap", () => {
+  class Foo {
+    constructor(public bar: number) {}
+  }
+  const foo1 = new Foo(1);
+  const foo2 = new Foo(2);
+
+  const weakMap = new WeakMap<Foo, number>();
+
+  assert.strictEqual(upsert(weakMap, foo1, updateFn, insertFn), 0);
+  assert.strictEqual(upsert(weakMap, foo1, updateFn, insertFn), 1);
+  assert.strictEqual(upsert(weakMap, foo2, updateFn, insertFn), 0);
+});
+
+test("No updateFn", () => {
+  const map = new Map<string, number>();
+
+  assert.strictEqual(upsert(map, "foo", undefined, insertFn), 0);
+  assert.strictEqual(upsert(map, "foo", undefined, insertFn), 0);
+});
+
+test("No insertFn", () => {
+  const map = new Map<string, number>();
+
+  assert.strictEqual(upsert(map, "foo", updateFn), undefined);
+  assert.strictEqual(map.has("foo"), false);
+
+  map.set("foo", 10);
+  assert.strictEqual(upsert(map, "foo", updateFn), 11);
+});
